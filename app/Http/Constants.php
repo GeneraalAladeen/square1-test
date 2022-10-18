9e716bcb7c0decb21554c0cffbdb11d55d7a48a6
GeneraalAladeen <32127065+GeneraalAladeen@users.noreply.github.com>
2022-10-18 12:07:40 +0100

    setup authentication with laravel breeze

---
+++ app/Http/Constants.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/**
+ * ============================================
+ * APP Roles
+ * ============================================
+ */
+const USER_ROLE = 'user';
+const ADMIN_ROLE = 'admin';
+
+const APP_ROLES = [
+    USER_ROLE,
+    ADMIN_ROLE
+];
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -4,11 +4,13 @@ namespace Tests\Feature\Auth;
 
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     public function test_registration_screen_can_be_rendered()
     {
@@ -20,8 +22,9 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'username' => $this->faker->username(),
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
